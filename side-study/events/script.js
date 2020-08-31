5dce6bdfb4530b1f2286fd1f768180f188de4fd6
philrussel21 <phil.antiporda21@gmail.com>
2020-08-31 18:51:10 +0930

    Adds impossible button challenge

--- side-study/events/script.js
+++ side-study/events/script.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('#clicker');
+// const btn = document.querySelector('#clicker');
 
 // btn.onclick = function () {
 //   alert('Hola!');
@@ -7,13 +7,29 @@ const btn = document.querySelector('#clicker');
 // addEventListener approach allows multiple callbacks to run on a specified event when that event happened to
 // the called upon object without overwriting the previous callback.
 
-btn.addEventListener('click', () => alert('Clicked!'));
-btn.addEventListener('click', () => console.log('Second thing!'))
+// btn.addEventListener('click', () => alert('Clicked!'));
+// btn.addEventListener('click', () => console.log('Second thing!'))
 
-btn.addEventListener('mouseover', function () {
-  btn.textContent = 'Stop hovering!'
-})
-btn.addEventListener('mouseout', function () {
-  btn.textContent = 'Click me too!'
+// btn.addEventListener('mouseover', function () {
+//   btn.textContent = 'Stop hovering!'
+// })
+// btn.addEventListener('mouseout', function () {
+//   btn.textContent = 'Click me too!'
+// })
+
+const btn = document.querySelector('#chaser')
+btn.addEventListener('mouseover', () => {
+  const magicXNum = Math.floor(Math.random() * window.innerWidth);
+  const magicYNum = Math.floor(Math.random() * window.innerHeight);
+  btn.style.left = `${magicXNum}px`;
+  btn.style.top = `${magicYNum}px`;
 })
 
+btn.addEventListener('click', () => {
+  btn.textContent = "Okay, You got me!"
+  document.body.style.backgroundColor = 'limegreen'
+})
+
+btn.addEventListener('mouseout', () => {
+  btn.textContent = "Catch me if you can!"
+})
