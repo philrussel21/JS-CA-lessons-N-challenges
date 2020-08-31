0f214b1bdc62f6f46fc9955f3990bbdf7359f796
philrussel21 <phil.antiporda21@gmail.com>
2020-08-31 21:25:50 +0930

    Updates to check for empty strings

--- side-study/key_events_with_TODOlist/script.js
+++ side-study/key_events_with_TODOlist/script.js
@@ -17,6 +17,9 @@ const ul = document.querySelector('#items');
 
 todoInput.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
+    // Checks to see if the value is an empty string
+    if (!this.value) return;
+    // Adds a new item to the list
     const newToDo = document.createElement('li');
     newToDo.textContent = this.value;
     ul.append(newToDo);
