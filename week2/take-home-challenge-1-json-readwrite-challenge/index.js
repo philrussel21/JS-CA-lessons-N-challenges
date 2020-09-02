110c6f00bbb5b8d0d6bbb1cc537c240342d5aca5
philrussel21 <phil.antiporda21@gmail.com>
2020-09-02 15:00:28 +0930

    Updates JSON challenge with passing tests

--- week2/take-home-challenge-1-json-readwrite-challenge/index.js
+++ week2/take-home-challenge-1-json-readwrite-challenge/index.js
@@ -1,10 +1,15 @@
-const fs = require('fs')
-const data
-
+const fs = require('fs');
+// const updatedFighters = 0;
 //Your Code Here
+let data = fs.readFileSync('./fighters.json');
+const fightersData = JSON.parse(data);
+const [...updatedFighters] = fightersData.fighters
+// updatedFighters.sort((a, b) => b.powerLevel - a.powerLevel);
+updatedFighters.pop()
+fs.writeFileSync('./updatedFighters.json', JSON.stringify(updatedFighters));
 
-
+console.log(fightersData.fighters);
 
 module.exports = {
     updatedFighters
-}
+};
