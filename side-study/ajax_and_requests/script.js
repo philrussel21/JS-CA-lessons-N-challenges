6a3487cb5e9ddd4ba9e67f81df8dc1801efd9ccb
philrussel21 <phil.antiporda21@gmail.com>
2020-09-04 16:58:49 +0930

    Updates api from jokesapi to GOTapi

--- side-study/ajax_and_requests/script.js
+++ side-study/ajax_and_requests/script.js
@@ -1,19 +1,22 @@
+// Extends the array proto to have a sample() that takes a random element from the arr
+Array.prototype.sample = function () {
+  const randNum = Math.floor(Math.random() * this.length);
+  return this[randNum];
+};
 
 // Making a request with the XMLHttpRequest object.
 const firstReq = new XMLHttpRequest();
+// Had to use function declaration instead of arrow function to achieve desired 
+// this behaviour
 firstReq.addEventListener('load', function () {
   console.log('WORKED!')
   const data = JSON.parse(this.responseText);
-  // Unpacks the setup and punchline property from the array of object inside data
-  for (let { setup, punchline } of data) {
-    console.log(setup);
-    console.log(punchline);
-    console.log("Baduum tssss")
-  }
+  console.log(data.aliases.sample())
+  console.log(`My name is ${data.name} also known as ${data.aliases.sample()}`)
 });
 firstReq.addEventListener('error', () => {
   console.log('ERROR!')
 });
-firstReq.open('GET', 'https://official-joke-api.appspot.com/random_ten');
+firstReq.open('GET', 'https://anapioficeandfire.com/api/characters/583');
 firstReq.send();
 console.log('Waiting for response');
