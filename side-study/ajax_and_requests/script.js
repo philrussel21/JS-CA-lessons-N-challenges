a7af84515a669a5e06f64e9e682fd45945eff524
philrussel21 <phil.antiporda21@gmail.com>
2020-09-04 21:19:03 +0930

    Initializes and basic use of axios library

--- side-study/ajax_and_requests/script.js
+++ side-study/ajax_and_requests/script.js
@@ -101,39 +101,54 @@ Array.prototype.sample = function () {
 
 
 // More on Fetch Chaining - cleaning up with named callback functions
-const firstPage = 'https://www.anapioficeandfire.com/api/characters?page=1&pageSize=10'
-const secondPage = 'https://www.anapioficeandfire.com/api/characters?page=2&pageSize=10'
+// const firstPage = 'https://www.anapioficeandfire.com/api/characters?page=1&pageSize=10'
+// const secondPage = 'https://www.anapioficeandfire.com/api/characters?page=2&pageSize=10'
 
-const checkStatusAndParse = (response) => {
-  // incoming object is going to be a response promise object that needs to be converted
-  // console.log(response);
-  if (!response.ok) throw Error(`Status Code Error: ${response.status}`);
-  // .json() method reads the Response object and returns a promise
-  // console.log(response.json())
-  // returns the read Response object as a promise that contains all the data
-  return response.json();
-}
+// const checkStatusAndParse = (response) => {
+//   // incoming object is going to be a response promise object that needs to be converted
+//   // console.log(response);
+//   if (!response.ok) throw Error(`Status Code Error: ${response.status}`);
+//   // .json() method reads the Response object and returns a promise
+//   // console.log(response.json())
+//   // returns the read Response object as a promise that contains all the data
+//   return response.json();
+// }
 
-const printNames = (data) => {
-  // retrieves the data that was passed from resolved of the previous promise
-  for (let record of data) {
-    console.log(record.aliases[0] || record.name)
-  }
-  // Automaticall returns a resolved Promise to keep the .then moving
-  return Promise.resolve(secondPage);
-}
+// const printNames = (data) => {
+//   // retrieves the data that was passed from resolved of the previous promise
+//   for (let record of data) {
+//     console.log(record.aliases[0] || record.name)
+//   }
+//   // Automaticall returns a resolved Promise to keep the .then moving
+//   return Promise.resolve(secondPage);
+// }
 
-const fetchMoreNames = (url) => {
-  return fetch(url)
-}
+// const fetchMoreNames = (url) => {
+//   return fetch(url)
+// }
 
-fetch(firstPage)
-  .then(checkStatusAndParse)
-  .then(printNames)
-  .then(fetchMoreNames)
-  .then(checkStatusAndParse)
-  .then(printNames)
+// fetch(firstPage)
+//   .then(checkStatusAndParse)
+//   .then(printNames)
+//   .then(fetchMoreNames)
+//   .then(checkStatusAndParse)
+//   .then(printNames)
+//   .catch((err) => {
+//     console.log('SOMETHING WENT WRONG');
+//     console.log(err);
+//   })
+
+// USING AXIOS
+// Does not need to parse the response anymore, this would straight up give you the 
+// Promise data right away.
+axios.get('https://www.anapioficeandfire.com/api/characters?page=1&pageSize=10')
+  .then((res) => {
+    const { data, headers } = res
+    console.log(data);
+    console.log(headers.link.split(','))
+  })
+  // Same with catch, no need to check the status or the ok properties, everything
+  // would be caught and an error would be thrown on catch.
   .catch((err) => {
-    console.log('SOMETHING WENT WRONG');
-    console.log(err);
+    console.log('SOMETHING WENT WRONG!', err)
   })
