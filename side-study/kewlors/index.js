a6e1964d8258726a1412a1e79089b3e5894b37dd
philrussel21 <phil.antiporda21@gmail.com>
2020-09-14 22:03:30 +0930

    Adds contrasting headings and default values onload

--- side-study/kewlors/index.js
+++ side-study/kewlors/index.js
@@ -4,57 +4,139 @@ const section = document.querySelector('.cont')
 const hexColourIn = document.querySelector('#hexColour')
 const rgbColourIn = document.querySelector('#rgbColour')
 const hslColourIn = document.querySelector('#hslColour')
+const baseDiv = document.querySelector('#base')
+const secDiv = section.querySelector('#secDiv')
 
+const thirdDiv = document.createElement('div')
+thirdDiv.classList.add('w-100', 'h-100')
+thirdDiv.setAttribute('id', 'thirdDiv')
 
-col.addEventListener('input', () => {
+window.onload = function () {
+  color.value = '#007FFF'
+  changeBgColor()
+}
+
+col.addEventListener('input', changeBgColor)
+
+theorySelect.addEventListener('input', (e) => {
+  if (e.target.value == 'com') {
+    if (section.children.length === 3) {
+      section.removeChild(section.lastElementChild)
+    }
+    changeBgColor()
+  }
+  else {
+    section.appendChild(thirdDiv)
+    changeBgColor()
+    // Fires off the event to change the bg color?
+  }
+})
+
+function changeBgColor() {
+  // converts to different color definitions
   const hexColor = col.value
   const { r, g, b } = hexToRgb(hexColor)
-  const { h, s, l } = rgb2hue(r, g, b)
+  const { h, s, l } = rgb2hsl(r, g, b)
+
+  // fills the input text with converted values
   hexColourIn.value = hexColor.toUpperCase()
   rgbColourIn.value = `rgb(${r}, ${g}, ${b})`
   hslColourIn.value = `hsl(${h}, ${s}%, ${l}%)`
 
+  // if Complementary Theory
   if (theorySelect.value === 'com') {
-    const div = section.querySelector('div');
+    baseDiv.style.backgroundColor = hexColor;
+
+    // clears existing headings
+    clearHeadings()
+    // adds new heading to the baseColor div
+    baseDiv.appendChild(addsBaseHeading())
+    shouldUseWhiteFont(baseDiv)
+
     const computedCompHue = computeComplementary(h)
     // changes the bgcolor of the div to the computed hsl colour
-    div.style.backgroundColor = `hsl(${computedCompHue}, ${s}%, ${l}%)`
-  }
+    secDiv.style.backgroundColor = `hsl(${computedCompHue}, ${s}%, ${l}%)`
+    // adds heading to the secondary div
+    secDiv.appendChild(addsCompHeading())
+    shouldUseWhiteFont(secDiv)
 
-})
-
-theorySelect.addEventListener('input', (e) => {
-  if (e.target.value == 'com') {
-    if (section.children.length === 2) {
-      section.removeChild(section.lastElementChild)
-    }
   }
+  // Analogous Theory
   else {
-    appendDivs(createDiv(1))
-    // Fires off the event to change the bg color?
+
+    // the secDiv becomes the baseDiv to put the base colour in the middle of the page
+    secDiv.style.backgroundColor = hexColor;
+
+    // clears existing headings
+    clearHeadings()
+    // adds new heading to highlight the baseColour div
+    secDiv.appendChild(addsBaseHeading())
+    shouldUseWhiteFont(secDiv)
+
+    // computes the analogous values according to basecolour the assigns to appropriate divs
+    const { first, second } = computeAnalogous(h)
+
+    baseDiv.style.backgroundColor = `hsl(${first}, ${s}%, ${l}%)`
+    baseDiv.appendChild(addsAnHeading())
+    shouldUseWhiteFont(baseDiv)
+
+
+    thirdDiv.style.backgroundColor = `hsl(${second}, ${s}%, ${l}%)`
+    thirdDiv.appendChild(addsAnHeading())
+    shouldUseWhiteFont(thirdDiv)
   }
-})
+
+}
+
+// algo to find the optimum colour of heading to maximise contrast
+function shouldUseWhiteFont(element) {
+  // element.style.backgroundColor returns a string in rgb format then r,g,b are extracted
+  const rgbArr = extractRGBfromString(element.style.backgroundColor)
+  const [, r, g, b] = rgbArr
+  const isWhite = (r * 0.299 + g * 0.587 + b * 0.114) < 186
+  isWhite ? element.style.color = "white" : element.style.color = "#333333"
+}
+
+function clearHeadings() {
+  baseDiv.innerHTML = ""
+  secDiv.innerHTML = ""
+  thirdDiv.innerHTML = ""
+}
+
+function addsBaseHeading() {
+  const h2 = document.createElement('h2')
+  h2.textContent = "Base Colour"
+  return h2
+}
+
+function addsCompHeading() {
+  const h2 = document.createElement('h2')
+  h2.textContent = "Complementary Colour"
+  return h2
+}
+
+function addsAnHeading() {
+  const h2 = document.createElement('h2')
+  h2.textContent = "Analogous Colour"
+  return h2
+}
+
+// returns an array with rgb,r,g,b as values
+function extractRGBfromString(rgb) {
+  var matchColors = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
+  var match = matchColors.exec(rgb);
+  return match
+}
+
+
+// COLOR THEORIES ALGO
 
 function computeComplementary(hue) {
   return (hue + 180) % 360
 }
 
-
-function createDiv(num) {
-  const divs = [];
-  for (let i = 0; i < num; i++) {
-    const newDiv = document.createElement('div')
-    newDiv.classList.add('w-100', 'h-100')
-    divs.push(newDiv)
-  }
-  return divs
-}
-
-function appendDivs(arr) {
-  const section = document.querySelector('.cont')
-  for (let div of arr) {
-    section.appendChild(div)
-  }
+function computeAnalogous(hue) {
+  return { first: (hue + 30) % 360, second: ((hue - 30) + 360) % 360 }
 }
 
 
@@ -73,7 +155,7 @@ function rgbToHex(r, g, b) {
 }
 
 
-function rgb2hue(r, g, b) {
+function rgb2hsl(r, g, b) {
   // Make r, g, and b fractions of 1
   r /= 255;
   g /= 255;
