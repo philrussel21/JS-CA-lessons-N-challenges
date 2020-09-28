8bb737f6a9fb1a337c4749679a1aed0bd9cebfbd
philrussel21 <phil.antiporda21@gmail.com>
2020-09-28 17:53:13 +0930

    Adds solution with handlebars as the template view engine

--- week6/day1/Pokemon/app.js
+++ week6/day1/Pokemon/app.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const fetch = require('node-fetch')
+const exphbs = require('express-handlebars')
+
 // express app
 const app = express()
 // listen for requests
 const server = app.listen(3000, () => console.log('Listening to Port 3000'))
 
+// handlebars
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
 
 
 app.get('/teamBuilder/:pokeNum', async (req, res) => {
   const pokeTeamNum = req.params.pokeNum
   const pokeTeam = await fetchTeam(pokeTeamNum)
-  res.send({ "data": pokeTeam })
+  // res.send({ "data": pokeTeam })
+  res.render('home', { pokeTeam: pokeTeam })
 })
 
 
@@ -30,8 +36,11 @@ async function fetchTeam(teamNum) {
   const promiseArr = []
   for (let i = 0; i < teamNum; i++) {
     promiseArr.push(fetchRandPokemon())
-    console.log(fetchRandPokemon())
   }
   const team = await Promise.all(promiseArr)
   return team
+}
+
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1)
 }
