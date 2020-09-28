e2037798bfae3ac766dd52ee7c3a2d5c3f1a2ac6
philrussel21 <phil.antiporda21@gmail.com>
2020-09-28 14:01:49 +0930

    Arranged file structure - Deletion

--- node/modules.js
+++
@@ -1,9 +0,0 @@
-
-const emp_staff = require('./people')
-
-console.log(emp_staff)
-
-
-const os = require('os')
-
-console.log(os.platform(), os.homedir())
--- node/people.js
+++
@@ -1,5 +0,0 @@
-const staff = ['leslie', 'ron', 'april', 'andy']
-
-const office = ['michael', 'jim', 'dwight', 'pam']
-
-module.exports = { staff, office }
