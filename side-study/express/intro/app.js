c9273a8e3f268454e61ef9de4ddfbc5ef2c20917
philrussel21 <phil.antiporda21@gmail.com>
2020-09-28 20:17:30 +0930

    Inits static data with template view engines

--- side-study/express/intro/app.js
+++ side-study/express/intro/app.js
@@ -16,14 +16,22 @@ app.get('/', (req, res) => {
   // res.send('<h1> Home Page <h1>')
 
   // Instead of writing the html, send the html file to instead
-  res.sendFile('./views/index.html', { root: __dirname })
+  // res.sendFile('./views/index.html', { root: __dirname })
+
+  // renders the index page found in views directory with the help of ejs
+  res.render('index')
 
 })
 
 app.get('/about', (req, res) => {
   // res.send('<h1>About Page<h1>')
 
-  res.sendFile('./views/about.html', { root: __dirname })
+  // res.sendFile('./views/about.html', { root: __dirname })
+  res.render('about')
+})
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create')
 })
 
 // Redirects
@@ -36,6 +44,7 @@ app.get('/about-me', (req, res) => {
 // This file runs from top to bottom, so it checks for any matching routes above
 // and only gets to this route IF no match is found, hence 404 Not Found
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname })
+  // res.status(404).sendFile('./views/404.html', { root: __dirname })
 
+  res.status(404).render('404')
 })
