5fa3dcf9372bce6fc4123832bba98f625f9d447b
philrussel21 <phil.antiporda21@gmail.com>
2020-10-12 14:10:24 +0930

    Inits tests and index js for cal challenge

--- week7/day2/calculator/index.js
+++ week7/day2/calculator/index.js
@@ -1,8 +1,34 @@
+function one(num = 1) {
+  return num
+}
 
-//Your code here
+function two(num = 2) {
+  return num
+}
+
+function three(num = 3) {
+  return num
+}
+
+function plus(num) {
+
+}
+
+function minus(num) {
+
+}
+
+function divide(num) {
+
+}
+
+function multiply(num) {
+
+}
 
 
 
+console.log(one())
 
 
 module.exports = {
--- week7/day2/calculator/index.test.js
+++ week7/day2/calculator/index.test.js
@@ -1,1 +1,34 @@
 const { one, two, three, plus, minus, divide, multiply } = require('./index')
+
+describe('Function return value', () => {
+  it('should return a whole number', () => {
+    expect(one() % 1).toBe(0)
+    expect(two() % 1).toBe(0)
+    expect(three() % 1).toBe(0)
+    expect(() => {
+      one(plus(one())) % 1
+    }).toBe(0)
+    expect(() => {
+      three(minus(two())) % 1
+    }).toBe(0)
+    expect(() => {
+      three(divide(two())) % 1
+    }).toBe(0)
+    expect(() => {
+      two(multiply(three())) % 1
+    }).toBe(0)
+  })
+})
+
+describe('Number Functions', () => {
+  it('should return a number when no args is passed', () => {
+    expect(one()).toBe(1)
+    expect(two()).toBe(2)
+    expect(three()).toBe(3)
+  });
+
+  it('should perform num operations based on args passed', () => {
+    expect(one(plus(one()))).toBe(2)
+    expect(two(multiply(three()))).toBe(6)
+  });
+});
