f73127345963eb8f626620da319d45c6c4194758
philrussel21 <phil.antiporda21@gmail.com>
2020-10-27 14:19:21 +0930

    Adds the yelling greeter challenge

--- week10/day2/buzzy-button/src/index.js
+++ week10/day2/buzzy-button/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './Button';
 import { HappyMessage } from './HappyMessage';
+import YellingGreeter from './YellingGreeter';
 
 const App = () => {
 	return (
 		<div>
-			<HappyMessage />
-			<Button />
+			<div>
+
+				<HappyMessage />
+			</div>
+			<div>
+
+				<Button />
+			</div>
+			<div>
+				<YellingGreeter message="WHAT THE FFFF" />
+			</div>
 		</div>
 	);
 };
