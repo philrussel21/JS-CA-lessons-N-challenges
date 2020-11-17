a97e7739d169cc97935afe4a2c398b6fbb774c42
philrussel21 <phil.antiporda21@gmail.com>
2020-11-17 14:33:34 +1000

    creates a new Context for BookList

--- week12/blog-app/src/components/BookList.js
+++ week12/blog-app/src/components/BookList.js
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
   const theme = isLightTheme ? light : dark;
+
+  const bookList = books.map(book => (
+    <li style={{ background: theme.ui }} key={book.id}>{book.title}</li>
+  ));
   return (
     <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
       <ul>
-        <li style={{ background: theme.ui }}>the way of kings</li>
-        <li style={{ background: theme.ui }}>the name of the wind</li>
-        <li style={{ background: theme.ui }}>the final empire</li>
+        {bookList}
       </ul>
     </div>
   );
