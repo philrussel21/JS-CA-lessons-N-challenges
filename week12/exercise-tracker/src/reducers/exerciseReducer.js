a82e1b6e12dded5160a797330006a872b042271d
philrussel21 <phil.antiporda21@gmail.com>
2020-11-17 17:41:31 +1000

    Adds preliminary reducer and set up initial component files

---
+++ week12/exercise-tracker/src/reducers/exerciseReducer.js
@@ -0,0 +1,32 @@
+export const ACTIONS = {
+  CREATE_EXERCISE: 'addExercise',
+  REMOVE_EXERCISE: 'removeExercise',
+  UPDATE_EXERCISE: 'updateExercise'
+};
+
+const newId = (arr) => {
+  return arr.length + 1;
+};
+
+export const exerciseReducer = (state, action) => {
+  // expecting an exercise object inside action as the new exercise
+  switch (action.type) {
+    case ACTIONS.CREATE_EXERCISE: {
+      return [...state, {
+        name: action.exercise.name,
+        duration: action.exercise.duration,
+        id: newId(state)
+      }];
+    }
+    // expecting an id passed as action.id to be removed from state
+    case ACTIONS.REMOVE_EXERCISE: {
+      const allOtherExercises = state.filter(exercise => exercise.id !== action.id);
+      return allOtherExercises;
+    }
+    // expecting an exercise object inside action
+    case ACTIONS.UPDATE_EXERCISE: {
+      const allOtherExercises = state.filter(exercise => exercise.id !== action.exercise.id);
+      return [allOtherExercises, action.exercise];
+    }
+  }
+};
